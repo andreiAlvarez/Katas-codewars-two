f76907f002c8897c74b915d9797f39d5c6113420
Andrei Alvarez <62512410+andreiAlvarez@users.noreply.github.com>
2021-02-04 20:23:40 -0500

    Update katas.js

--- katas.js
+++ katas.js
@@ -88,3 +88,18 @@ scoobydoo = snoopy = {bark: String.bind(1, 'Woof')};
 // kata 9
 
 const isLockNessMonster = s => /tree fiddy| three fifty|3.50/.test(s);
+
+// kata 10 
+
+function twentyOne(...arr) {
+  let deck = ["2", "3", "4", "5", "6", "7", "8", "9", "1", "A"];
+  let deck2 = ["J", "Q", "K"];
+  let sum = arr
+    .map(v =>
+      deck.indexOf(v.slice(0, 1)) > -1
+        ? deck.indexOf(v.slice(0, 1)) + 2
+        : deck2.indexOf(v.slice(0, 1)) + 2
+    )
+    .reduce((a, b) => a + b, 0);
+  return sum === 21 ? "twenty-one" : sum < 21 ? "less" : "more";
+}
